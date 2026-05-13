6ec89e5f51fa3b31730e724a942f92dbff577068
RinaShimazu <2026opnewemp007@fullness.email>
2026-05-13 13:17:34 +0900

    5/13

--- Infrastructures/Accessors/EmployeeAccessor.cs
+++ Infrastructures/Accessors/EmployeeAccessor.cs
@@ -1,21 +1,13 @@
 using CS_DB_Exercise.Infrastructures.Entities;
 using CS_DB_Exercise.Infrastructures.Contexts;
+
 namespace CS_DB_Exercise.Infrastructures.Accessors;
-/// <summary>
-/// departmentテーブルにアクセスするクラス
-/// </summary>
-/// <author>Fullness,Inc.</author>
-/// <date>2025-11-16</date>
-/// <version>1.0.0</version>
+
 public class EmployeeAccessor
 {
-    //  アプリケーション用DbContext
     private readonly AppDbContext _context;
 
-    /// <summary>
-    /// コンストラクタ
-    /// </summary>
-    /// <param name="context">アプリケーション用DbContext</param>
+
     public EmployeeAccessor(AppDbContext context)
     {
         _context = context;
@@ -31,10 +23,10 @@ public class EmployeeAccessor
         return employees;
     }
 
-    public EmployeeEntity? FindByDeptId(int deptId)
+    public DepartmentEntity? FindByDeptId(int deptId)
     {
         // Find()メソッドを使用して、指定した部署Idの部署を取得する
-        var dept_id = _context.Departments.Find(deptId);
-        return dept_id;
+        var dept = _context.Departments.Find(deptId);
+        return dept;
     }
 }
